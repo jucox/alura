9fa0b18190354b10603b4973214402b27369d7c4
Renato Junior <renatojr.dev@gmail.com>
2023-01-03 23:05:22 -0300

    aluramidi started

---
+++ main.js
@@ -0,0 +1,25 @@
+const keysList = document.querySelectorAll('.tecla');
+
+for (let i = 0; i < keysList.length; i++) {
+    const key = keysList[i];
+    const instrument = key.classList[1];
+    const keyAudioId = `#som_${instrument}`;
+    
+    key.onclick = function () {
+        playSound(keyAudioId);
+    }
+
+    key.onkeydown = function (event) {
+        if (event.code === 'Space' || event.code === 'Enter') {
+            key.classList.add('ativa')
+        }
+    }
+
+    key.onkeyup = function () {
+        key.classList.remove('ativa')
+    }
+}
+
+function playSound (keyAudioId) {
+    document.querySelector(keyAudioId).play();
+}
